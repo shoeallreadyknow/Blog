55d34359917ecc46aea95936cd0e3dd90070b36b
justin landaverde <justlandaverde28@gmail.com>
2014-11-18 15:08:24 -0800

    finished 4-6

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+            echo"Web Sever is Running Locally"
         ?>
     </body>
 </html>
