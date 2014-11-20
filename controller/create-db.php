3a3eaac93e140a4ffcd09c5855df2b6c5a83ccf4
justin landaverde <justlandaverde28@gmail.com>
2014-11-20 15:11:32 -0800

    set up new variables and connected to local host

---
+++ controller/create-db.php
@@ -0,0 +1,8 @@
+<?php
+/* it is getting the info from the other file and puting it into this file
+ */
+  require_once('../model/database.php');
+ //conecting to mysql database
+  $connection = new mysqli($host, $user, $password);
+ 
+
---
+++ model/database.php
@@ -0,0 +1,8 @@
+<?php
+/*setting all the variables that wee need to connect to local host database
+ */
+$host = "localhost";
+$username= "root";
+ $password = "root";
+ $database = "blog_db";
+
